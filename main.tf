66b7d5fc2ae63e7088dc1d62ae53851bf5dc7f61
mmayurshinde <61413001+mmayurshinde@users.noreply.github.com>
2024-02-26 16:39:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "example" {
 
 provider "aws" {
   region     = "ap-southeast-2"
-  access_key = ""
-  secret_key = ""
+  access_key = "${env.AWS_ACCESS_KEY_ID}"
+  secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
 }
 
 
