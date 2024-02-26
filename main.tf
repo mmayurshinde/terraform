11f01b6573ffb03c84c47d6be83def11c79b447f
mmayurshinde <61413001+mmayurshinde@users.noreply.github.com>
2024-02-26 17:45:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,6 @@ resource "aws_instance" "example" {
   key_name      = "key-value-terra"
 
 
-provider "aws" {
-  region     = "ap-southeast-2"
-// accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-// secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-}
-
-
   tags = {
     Name = "LaunchEC2"  
   }
